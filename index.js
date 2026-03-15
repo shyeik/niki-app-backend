edeb065b776ca77385c7baec96a18b0d921194c9
Jake Declock <derilojake2528@gmail.com>
2026-03-16 01:24:47 +0800

    ssss

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use("/api/users", userRoutes)
 
 // Auth API
 const authRoutes = require("./routes/auth")
-app.use("api/auth", authRoutes)
+app.use("/api/auth", authRoutes)
 
 // Start server
 const PORT = process.env.PORT || 8080
