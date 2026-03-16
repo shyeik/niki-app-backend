6a08bd81a65db53204049189409a516b60611d77
Jake Declock <derilojake2528@gmail.com>
2026-03-16 22:19:24 +0800

    fix routes before connecting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
-require("dotenv").config() // optional, for .env
+require("dotenv").config()
 
 const app = express()
 
@@ -9,35 +9,29 @@ const app = express()
 app.use(cors({
   origin: process.env.CLIENT_ORIGIN,
   credentials: true,
-}));
+}))
 
 app.use(express.json())
 
-
-// MongoDB connection
-const MONGO_URI = process.env.MONGO_URI
-
-mongoose.connect(MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err))
-
-// Test route
-app.get("/", (req, res) => {
-  res.send("NIKI API Server is Running")
-})
-
-
-
-// Users API
+// Routes
 const userRoutes = require("./routes/users")
-app.use("/api/users", userRoutes)
-
-// Auth API
 const authRoutes = require("./routes/auth")
+
+app.use("/api/users", userRoutes)
 app.use("/api/auth", authRoutes)
 
-// Start server
 const PORT = process.env.PORT || 8080
-app.listen(PORT, () => {
-  console.log(`Server running on https://localhost:${PORT}`)
-})
+
+// Start server AFTER MongoDB connects
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected")
+
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`)
+    })
+
+  })
+  .catch(err => {
+    console.error("MongoDB connection error:", err)
+  })
